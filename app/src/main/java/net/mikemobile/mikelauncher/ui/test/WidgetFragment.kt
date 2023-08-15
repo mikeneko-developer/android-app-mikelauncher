6bfc7d6e1b480722b01bcd0d32911ff0b227323a
mikeneko-developer <mikemobiler.developer@gmail.com>
2023-08-15 17:08:35 +0900

    とりあえず保存

--- app/src/main/java/net/mikemobile/mikelauncher/ui/test/WidgetFragment.kt
+++ app/src/main/java/net/mikemobile/mikelauncher/ui/test/WidgetFragment.kt
@@ -1,110 +1,89 @@
-package net.mikemobile.mikelauncher
+package net.mikemobile.mikelauncher.ui.test
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import net.mikemobile.mikelauncher.ui.applist.AppListFragment
-import net.mikemobile.mikelauncher.ui.main.MainFragment
 import android.appwidget.AppWidgetHost
 import android.appwidget.AppWidgetHostView
-
 import android.appwidget.AppWidgetManager
-
-import android.graphics.drawable.Drawable
-
 import android.appwidget.AppWidgetProviderInfo
-import android.content.ActivityNotFoundException
-
-import androidx.core.app.ActivityCompat.startActivityForResult
-
 import android.content.Intent
 import android.os.Build
+import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import net.mikemobile.android.view.CellLayout
 import net.mikemobile.android.view.DragLayer
-import net.mikemobile.android.view.Folder
-import net.mikemobile.android.view.Workspace
+import net.mikemobile.mikelauncher.MainActivity
+import net.mikemobile.mikelauncher.R
+import net.mikemobile.mikelauncher.ui.main.MainFragment
+import net.mikemobile.mikelauncher.ui.main.MainViewModel
 import java.lang.NullPointerException
 
-
-class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClickListener {
-
-    val APPWIDGET_HOST_ID = 1024
-    var dragLayer: DragLayer? = null
-    var mWorkspace: FrameLayout? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-
-        if (savedInstanceState == null) {
-            //setFragment(MainFragment.newInstance())
-            //setFragment(AppListFragment.newInstance())
-        }
-
-        dragLayer = findViewById(R.id.dragLayer) as DragLayer
+class WidgetFragment : Fragment(), View.OnLongClickListener {
 
 
-        mWorkspace = findViewById(R.id.workspace) as FrameLayout
+    //private lateinit var viewModel: MainViewModel
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_widget, container, false)
 
-        val button = findViewById(R.id.button) as Button
+        dragLayer = view.findViewById(R.id.dragLayer) as DragLayer
+        mWorkspace = view.findViewById(R.id.workspace) as FrameLayout
+
+        val button = view.findViewById(R.id.button) as Button
         button.setOnClickListener {
             openWidget()
         }
-        val button2 = findViewById(R.id.button2) as Button
+        val button2 = view.findViewById(R.id.button2) as Button
         button2.setOnClickListener {
             removeWidget()
         }
-        val button3 = findViewById(R.id.button3) as Button
+        val button3 = view.findViewById(R.id.button3) as Button
         button3.setOnClickListener {
             updateWidget()
         }
 
+        return view
+    }
 
-        mAppWidgetHost = AppWidgetHost(applicationContext, XXXX)    //…(1)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        mAppWidgetHost = AppWidgetHost(this.requireActivity().applicationContext, XXXX)    //…(1)
         mAppWidgetHost!!.startListening()
 
         for (id in mAppWidgetHost!!.appWidgetIds) {
             Log.i("TESTTEST", "onCreate  id:" + id)
             mAppWidgetHost!!.deleteAppWidgetId(id) //…(8)
         }
-
     }
 
-    var mAppWidgetHost: AppWidgetHost? = null
-    override fun onResume(){
-        super.onResume()
-    }
     override fun onDestroy(){
         super.onDestroy()
         try {
             mAppWidgetHost?.stopListening()
         } catch (ex: NullPointerException) {
             Log.w(
-                LOG_TAG,
+                MainActivity.LOG_TAG,
                 "problem while stopping AppWidgetHost during Launcher destruction",
                 ex
             )
         }
     }
 
-    private fun setFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.container,
-            fragment
-        ).commitNow()
-    }
 
-    /**
-    override fun finish() {
-    }
-    */
+    var dragLayer: DragLayer? = null
+    var mWorkspace: FrameLayout? = null
+    var mAppWidgetHost: AppWidgetHost? = null
 
     val XXXX = 111111
 
@@ -117,11 +96,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClick
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (resultCode) {
-            RESULT_OK -> when (requestCode) {
+            AppCompatActivity.RESULT_OK -> when (requestCode) {
                 REQUEST_CODE_ADD_APPWIDGET -> {
                     val appWidgetId = data!!.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)
                     val appWidgetProviderInfo =
-                        AppWidgetManager.getInstance(applicationContext).getAppWidgetInfo(appWidgetId) //…(2)
+                        AppWidgetManager.getInstance(this.requireActivity().applicationContext).getAppWidgetInfo(appWidgetId) //…(2)
 
                     //…(3)
                     if (appWidgetProviderInfo.configure != null) {
@@ -132,7 +111,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClick
 
                         //…(4)
                     } else {
-                        onActivityResult(REQUEST_CODE_ADD_APPWIDGET_2, RESULT_OK, data)
+                        onActivityResult(REQUEST_CODE_ADD_APPWIDGET_2,
+                            AppCompatActivity.RESULT_OK, data)
                     }
                 }
                 REQUEST_CODE_ADD_APPWIDGET_2 -> {
@@ -140,9 +120,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClick
                     //…(6)
                     val appWidgetId2 = data!!.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)
                     val appWidgetProviderInfo2 =
-                        AppWidgetManager.getInstance(applicationContext).getAppWidgetInfo(appWidgetId2)
+                        AppWidgetManager.getInstance(this.requireActivity().applicationContext).getAppWidgetInfo(appWidgetId2)
                     val widgetLabel = appWidgetProviderInfo2.label
-                    val widgetIcon = packageManager.getDrawable(
+                    val widgetIcon = this.requireActivity().packageManager.getDrawable(
                         appWidgetProviderInfo2.provider.packageName,
                         appWidgetProviderInfo2.icon,
                         null
@@ -153,7 +133,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClick
 
                     appWidgetId = appWidgetId2
 
-                    var hostView = mAppWidgetHost!!.createView(applicationContext, appWidgetId2, appWidgetProviderInfo2)
+                    var hostView = mAppWidgetHost!!.createView(this.requireActivity().applicationContext, appWidgetId2, appWidgetProviderInfo2)
 
                     //hostView.setMinimumHeight(appWidgetProviderInfo2.minHeight)
                     if (Build.VERSION.SDK_INT > 15) {
@@ -166,10 +146,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClick
 
                 }
             }
-            RESULT_CANCELED -> when (requestCode) {
+            AppCompatActivity.RESULT_CANCELED -> when (requestCode) {
                 REQUEST_CODE_ADD_APPWIDGET, REQUEST_CODE_ADD_APPWIDGET_2 -> if (data != null) {
                     val appWidgetId = data.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)
-                    val appWidgetHost = AppWidgetHost(this, XXXX)
+                    val appWidgetHost = AppWidgetHost(this.requireContext(), XXXX)
                     if (appWidgetId != -1) appWidgetHost.deleteAppWidgetId(appWidgetId) //…(8)
                 }
             }
@@ -228,71 +208,35 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, View.OnLongClick
         mWorkspace!!.removeAllViews()
     }
 
+    private val mDesktopLocked = true
+    fun isWorkspaceLocked(): Boolean {
+        return mDesktopLocked
+    }
+
+    override fun onLongClick(v: View?): Boolean {
+        return false
+    }
+
+
+
     companion object {
+        fun newInstance() = WidgetFragment()
+
         const val APPWIDGET_HOST_ID = 1024
         const val LOG_TAG = "Launcher"
         const val DEFAULT_SCREN = 1
         private val sLock = Any()
         private var sScreen: Int = DEFAULT_SCREN
 
-        fun setScreen(screen: Int) {
-            synchronized(net.mikemobile.mikelauncher.MainActivity.sLock) {
-                net.mikemobile.mikelauncher.MainActivity.sScreen = screen
-            }
-        }
-
-        fun getScreen(): Int {
-            synchronized(net.mikemobile.mikelauncher.MainActivity.sLock) {
-                return net.mikemobile.mikelauncher.MainActivity.sScreen }
-        }
-    }
-
-    private val mDesktopLocked = true
-    fun isWorkspaceLocked(): Boolean {
-        return mDesktopLocked
-    }
-
-
-    override fun onClick(p0: View?) {
-
-    }
-
-    fun startActivitySafely(intent: Intent) {
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        try {
-            startActivity(intent)
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(this, R.string.activity_not_found, Toast.LENGTH_SHORT).show()
-        } catch (e: SecurityException) {
-            Toast.makeText(this, R.string.activity_not_found, Toast.LENGTH_SHORT).show()
-            Log.e(
-                LOG_TAG,
-                "Launcher does not have the permission to launch " + intent +
-                        ". Make sure to create a MAIN intent-filter for the corresponding activity " +
-                        "or use the exported attribute for this activity.",
-                e
-            )
-        }
-    }
-
-    private fun closeFolder() {
-        /**
-        val folder: Folder = mWorkspace!!.getOpenFolder()
-        if (folder != null) {
-            closeFolder(folder)
-        }
-        }*/
-    }
-
-    fun closeFolder(folder: Folder) {
-        //folder.getInfo().opened = false
-        //val parent = folder.getParent() as ViewGroup
-        //parent?.removeView(folder)
-        //folder.onClose()
-    }
-
-    override fun onLongClick(p0: View?): Boolean {
-
-        return false
+//        fun setScreen(screen: Int) {
+//            synchronized(net.mikemobile.mikelauncher.MainActivity.sLock) {
+//                net.mikemobile.mikelauncher.MainActivity.sScreen = screen
+//            }
+//        }
+//
+//        fun getScreen(): Int {
+//            synchronized(net.mikemobile.mikelauncher.MainActivity.sLock) {
+//                return net.mikemobile.mikelauncher.MainActivity.sScreen }
+//        }
     }
 }
